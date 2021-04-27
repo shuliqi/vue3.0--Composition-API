2e188e22eb9dc5533d8ffe9ac8c7e5cb35922d91
shuliqi <shuliqi@360.cn>
2021-04-27 17:37:47 +0800

    watchEffect

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import { createApp } from 'vue'
 // import App from './reactive.vue'
 // import App from './isRef.vue'
 // import App from './toRefs.vue'
-import App from './watch.vue'
+// import App from './watch.vue';
+import App from './watchEffect.vue'
 createApp(App).mount('#app')
---
+++ src/watchEffect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>watchEffect</div>
+</template>
+<script>
+import { ref, watchEffect } from "vue";
+export default {
+  setup () {
+    const name = ref("shuliqi");
+    const age = ref(0);
+    setTimeout(() => {
+      name.value = "张大大";
+      age.value = 20;
+    }, 1000);
+    const stop = watchEffect(() => {
+      console.log("name值：", name.value, "age值：", age.value);
+    });
+    // 停止监听
+    stop();
+  }
+}
+</script>
