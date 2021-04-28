fdadc72c6c7b69a6070a4ff7500b9c72a3f59254
shuliqi <shuliqi@360.cn>
2021-04-28 17:10:07 +0800

    生命周期

---
+++ src/Lifecycle.vue
@@ -0,0 +1,39 @@
+<teamlate>
+</teamlate>
+<script>
+  import { 
+    onBeforeMount,
+    onMounted,
+    onBeforeUpdate,
+    onUpdated,
+    onBeforeUnmount,
+    onUnmounted,
+    onErrorCaptured, 
+    onRenderTracked,
+    onRenderTriggered
+ }  from "vue";
+  export default {
+    beforeCreate () {
+      console.log("beforecreate")
+    },
+    created () {
+      console.log("created")
+    },
+    setup () {
+      onBeforeMount(() => {
+        console.log("Composition API: onBeforeMount")
+      });
+
+      onMounted(() => {
+        console.log("Composition API: onMounted")
+      });
+
+      onBeforeUpdate(() => {
+        console.log("Composition API: onBeforeUpdate")
+      })
+      // ...
+
+    },
+    // ...
+  }
+</script>
